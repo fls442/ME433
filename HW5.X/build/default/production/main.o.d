f8ecd2d495e5175d978646f8d159f78f7a2d9953
fls442 <129420860+fls442@users.noreply.github.com>
2023-05-07 19:21:13 -0500

    HW5 DONE
    LFG BABIEEEEE

--- HW5.X/build/default/production/main.o.d
+++ HW5.X/build/default/production/main.o.d
@@ -1,3 +1,5 @@
-build/default/production/main.o: main.c nu32dip.h
+build/default/production/main.o: main.c nu32dip.h spi.h
 
 nu32dip.h:
+
+spi.h:
--- HW5.X/main.c
+++ HW5.X/main.c
@@ -1,57 +1,54 @@
 #include "nu32dip.h"    // constants, functions for startup and UART
 #include <math.h>
-//#include "spi.h"
+#include "spi.h"
 
 #define PI 3.14159265
-#define POINTS 100          // data points per sine wave
 
 void delay(int ms);
 
 int main(void) {
   unsigned int vsin, vtri, i=0;
-  float rads, prog, amp=1023/2;
-  unsigned short a=0b0111<<12, b=0b1111<<12, send=0;
+  double rads, prog, amp=511.5, points=100, m=20.46;
+  unsigned short a=0b0111<<12, b=0b1111<<12, senda=0, sendb=0;
   
   NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
-  //initSPI();
+  initSPI();
  
   TRISBbits.TRISB6 = 0;          //PIN B6 flips for debugging
   LATBbits.LATB6 = 1;            //
   
   while (1) {
-      if(i>POINTS){
+      if(i>points){
           i=0;
       }
-      prog = i/POINTS;
-      rads = (2*PI)*prog;
-      if(i%2==0){
-          LATBbits.LATB6 = 0;
-      }
-      else{
-          LATBbits.LATB6 = 1;
-      }
+      rads = (2*PI)*(i/points);
       
-      // calc sine voltage (0-1023))
-      vsin = amp + amp*sin(rads);
-      // build sine SPI bits for channel A
-      send = a|(vsin<<2);
+//      if(i%2==0){
+//          LATBbits.LATB6 = 0;
+//      }
+//      else{
+//          LATBbits.LATB6 = 1;
+//      }
+      
+     
+      vsin = amp + amp*sin(rads);    // calc sine voltage (0-1023))
+      senda = a|(vsin<<2);           // build sine SPI bits for channel A
       // send sine SPI
-      //sendit(send);
+      sendit(senda);
       
       // calc tri voltage
-      if(i<50){
-          vtri = prog*2*amp;
-      }
-      else{
-          vtri = (-1*prog*2*amp) + (2*amp);
-      }
+      if(i<50){vtri = m*i;}
+      else{vtri = (-1*m*i) + (4*amp);}
+      if(vtri<0){vtri=1023;}
+      if(vtri>1023){vtri=0;}
+      
       // build tri SPI bits
-      send = a|(vtri<<2);
+      sendb = b|(vtri<<2);
       // send tri SPI
-      //sendit(send);
+      sendit(sendb);
       //delay 5 ms to maintain 200 pts/second for 2Hz sine wave
       i++; 
-      delay(5);
+      delay(3);
   }
 }
 
@@ -59,5 +56,5 @@ int main(void) {
 //delay in mili seconds
 void delay(int ms){
     _CP0_SET_COUNT(0);
-    while (_CP0_GET_COUNT() <(ms*24*10^3)){;} //do nothing
+    while (_CP0_GET_COUNT() <(ms*24*pow(10, 3))){;} //do nothing
 }
